181d95d800370262db3bdab6f6ccde43110dc321
ana.silva69 <ana.silva69@ITELABD10>
2022-05-19 21:03:11 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, e, i, o, u;
+            string letra;
+            
+            Console.WriteLine("escolha uma letra");
+            letra = Console.ReadLine();
+
+
+            if (letra == "a" ||letra == "e" ||letra == "i" ||letra == "o" ||letra == "u") 
+            {
+                Console.WriteLine("Essa letra é uma vogal!");
+            }
+            else 
+            {
+                Console.WriteLine("Essa letra é uma consoante!");
+            }
+                
+
+            
+
+
+        }
+    }
+}
